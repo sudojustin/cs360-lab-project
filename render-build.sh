c0a3eeb0a761c2464f4039b248b4a16b62faea02
sudojustin <120149080+sudojustin@users.noreply.github.com>
2025-04-20 11:39:55 -0700

    Add render deployment script

---
+++ render-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install PHP & dependencies
+apt-get update
+apt-get install -y php php-mbstring php-xml php-bcmath php-curl php-sqlite3 unzip curl git
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Laravel setup
+composer install --no-dev
+cp .env.example .env
+php artisan key:generate
+touch /tmp/database.sqlite
+php artisan migrate --force
